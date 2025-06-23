a85d58e1377ec861d062df095bdb83ae65b405a7
soraemonQu <salt110.9u@gmail.com>
2025-06-23 09:24:55 +0900

    Revise Test

--- assignments/1/q3.c
+++ assignments/1/q3.c
@@ -7,13 +7,19 @@ double convC2F(double c)
 
 int main()
 {
-    printf("%.1f\n", convC2F(20.0));
-    /* expects
-        68.0
-    */
-    printf("%.1f\n", convC2F(0.0));
-    /* expects
-        32.0
-    */
+    double inputs[] = {20.0, 0.0};
+    double expected[] = {68.0, 32.0};
+
+    int l = sizeof(inputs) / sizeof(inputs[0]);
+    for (int i = 0; i < l; ++i)
+    {
+        double result = convC2F(inputs[i]);
+        if (result == expected[i])
+            fprintf(stdout, "CASE %i/%i : OK\n", i + 1, l);
+        else
+            fprintf(stderr, "CASE %i/%i : NG <expected %.1f, got %.1f>\n", i + 1, l, expected[i], result);
+    }
+    printf("\n");
+
     return 0;
 }
